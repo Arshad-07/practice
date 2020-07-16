d023f80fb49732b2212f2a927441ee3b6788de25
Arshad Syed <Arshad Syed@192.168.0.105>
2020-07-16 15:55:58 +0530

    version 1.0.1

--- Practiceadaptive/src/pracadapt/Cerifications.java
+++ Practiceadaptive/src/pracadapt/Cerifications.java
@@ -1,10 +1,25 @@
 package pracadapt;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Cerifications {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver","E:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://shop.adaptiveus.com/");
+		driver.manage().window().maximize();
+		WebElement acceptcookie=driver.findElement(By.id("hs-eu-confirmation-button"));
+		if(acceptcookie.isDisplayed())
+		{
+			acceptcookie.click();
+		}
+		Thread.sleep(5000);
+		driver.close();
 	}
+	
 
 }
